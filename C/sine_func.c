882e36a61f20f62ef4dbff0e5dafde23528e45eb
Karthikeyan <74821406+karthi802@users.noreply.github.com>
2021-01-27 14:38:06 +0530

    Create sine_func.c

---
+++ C/sine_func.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+  int i,j,n,fact,sign = -1;
+  float x,p,sum=0;
+  
+  scanf("%f", &x);
+  scanf("%d", &n);
+  
+  for(i=1; i<=n; i+=2)
+  {
+    p=1;
+    fact=1;
+    for(j=1; j<=1; j++)
+    {
+      p = p*x;
+      fact = fact*j;
+    }
+    sign = -1*sign;
+    sum += sign*p/fact;
+  }
+  
+  printf("%f", sum);
+  
+  return 0;
+ }
