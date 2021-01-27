cc43ade32cb81f7baccfa6319c2744d48d4e455e
Karthikeyan <74821406+karthi802@users.noreply.github.com>
2021-01-27 14:43:37 +0530

    Create reversing_digit.c

---
+++ C/reversing_digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,rev=0,rem;
+    
+    scanf("%d",&a);
+    
+    while(a!=0)
+    {
+        rem = a%10;
+        rev = rev*10 + rem;
+        a/=10;
+    }
+    printf("%d",rev);
+    
+    return 0;
+}
