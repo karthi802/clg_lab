65673cbf15c9894021c0f503b8d80b26f10b7890
karthi802 <karthi2003ind@gmail.com>
2021-06-01 19:52:03 +0530

    reordered and created sem2 folder

---
+++ sem1/C++/size_of_datatypes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+    
+    return 0;
+}
